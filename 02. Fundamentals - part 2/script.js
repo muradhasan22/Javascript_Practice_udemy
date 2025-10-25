26e3bf9f2993b571c9904436e2a69640db527bc7
muradhasan22 <muradhasan019922@gmail.com>
2025-10-26 00:10:48 +0600

    start fundamental part 2 with strict mode

---
+++ 02. Fundamentals - part 2/script.js
@@ -0,0 +1,18 @@
+//activating strict mode with
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+console.log(hasDriversLicense);
+
+// Function
+// A function is a block of reusable code that performs a specific task.
+
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+}
+greetUser("Elmi");
+
