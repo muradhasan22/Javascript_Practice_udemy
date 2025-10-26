2fdf31dfa782436e865e471f944546ea767bb43a
muradhasan22 <muradhasan019922@gmail.com>
2025-10-26 12:17:44 +0600

    function declaration , function expression & arrow function

--- 02. Fundamentals - part 2/script.js
+++ 02. Fundamentals - part 2/script.js
@@ -19,8 +19,31 @@ greetUser("Elmi");
 //explanation: name is parameter. When call/invoke the function, i pass the argument("Elmi").
 
 //Function Declaration :
-
-
+function calcAge1(birthyear) {
+    return 2037 - birthyear;
+}
+const age1 = calcAge1(1991);
 
 //Function Expression:
+/*what we can with function expression: 
+** store them in variable,
+** pass them to other function
+** retrurn them from function */
 
+const calcAge2 = function (birthyear) {
+    return 2037 - birthyear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+//Arrow function :
+
+const yearsUntilretirement = (birthYear, firstname) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+
+    return `${firstname} retires in ${retirement} years .`
+}
+console.log(yearsUntilretirement(1991, 'Murad'));
+console.log(yearsUntilretirement(1980, 'Murad'));
